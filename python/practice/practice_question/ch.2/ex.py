03348744335f230573c2aad730d1555f54246293
lkkcleo <leowonkinhang@gmail.com>
2025-04-19 19:13:25 +0800

    do some ex and read some book

---
+++ python/practice/practice_question/ch.2/ex.py
@@ -0,0 +1,26 @@
+#1. True / False
+#2. and / or / not
+#4 F F T F F T
+#5 == != > < <= >=
+#9
+def q9(spam):
+    if spam ==1 :
+        print('Hello')
+    elif spam== 2:
+        print('Howdy')
+    else :
+        print("Greetings")
+#10 ctrl+c
+#11 break break the loop, continue back to the condition checking
+#12 all same
+#13
+def q13():
+    for i in range(10):
+        print(i+1)
+    i=0
+    while i<10:
+        i+=1
+        print(i)
+#14 spam.bacon()
+print(123)
+q13()
